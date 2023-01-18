43c5cfa728f2848b82704330ed46ee529b6f611b
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-01-17 21:27:05 -0500

    cardinalidad

---
+++ desarrollo-web-juan/appsalon/07-cardinalidad.sql
@@ -0,0 +1,9 @@
+/* ---------------------------------- */
+/*            Cardinalidad            */
+/* ---------------------------------- */
+/* La cardinalidad se refiere al número máximo de veces que una instancia se relaciona con otra.
+ No pienses en esto como un número obligatorio, sino el máximo de veces que 2 tablas se pueden relacionar
+ */
+SHOW TABLES;
+
+DROP TABLE reservaciones;
